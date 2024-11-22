595e0f31e9e743a6812b84cca249d7ab15584983
Viviorigi <duong682@gmail.com>
2024-11-22 11:07:41 +0700

    isolation unit test

--- src/test/java/com/duong/identityservice/controller/UserControllerTest.java
+++ src/test/java/com/duong/identityservice/controller/UserControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -25,6 +26,7 @@ import java.time.LocalDate;
 @Slf4j
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource("/test.properties")
 public class UserControllerTest {
 
     @Autowired
--- src/test/java/com/duong/identityservice/service/UserServiceTest.java
+++ src/test/java/com/duong/identityservice/service/UserServiceTest.java
@@ -13,10 +13,13 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestPropertySource;
+
 import static  org.junit.jupiter.api.Assertions.assertThrows;
 import java.time.LocalDate;
 
 @SpringBootTest
+@TestPropertySource("/test.properties")
 public class UserServiceTest {
     @Autowired
     private UserService userService;
