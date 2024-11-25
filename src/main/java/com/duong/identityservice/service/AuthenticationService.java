f41bc4606a0ae561a99a9317d7a68cfff376c1d1
Viviorigi <duong682@gmail.com>
2024-11-25 16:40:42 +0700

    config profile, environment variables

--- src/main/java/com/duong/identityservice/service/AuthenticationService.java
+++ src/main/java/com/duong/identityservice/service/AuthenticationService.java
@@ -58,6 +58,7 @@ public class AuthenticationService {
     protected long REFRESHABLE_DURATION;
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        log.info("SignKey: {}", SIGNER_KEY);
         var user = userRepository
                 .findByUsername(request.getUsername())
                 .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXISTED));
